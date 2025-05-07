b5d85d99d3b1b2748fb0126632ad5756e8457def
James Gould <james-gould@users.noreply.github.com>
2025-05-07 20:02:01 +0100

    Fixed package version for .NET 9 sample project

--- KeyVaultEmulatorSample/KeyVaultEmulatorSample.AppHost/Program.cs
+++ KeyVaultEmulatorSample/KeyVaultEmulatorSample.AppHost/Program.cs
@@ -1,15 +1,15 @@
-using AzureKeyVaultEmulator.Hosting.Aspire;
+using AzureKeyVaultEmulator.Aspire.Hosting;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
 // Redirect existing resource to the emulator:
 
-//var keyVault = builder
-//    .AddAzureKeyVault("keyvault")
-//    .RunAsEmulator();
+var keyVault = builder
+    .AddAzureKeyVault("keyvault")
+    .RunAsEmulator();
 
 // Or directly create it without an existing Azure tenancy
-var keyVault = builder.AddAzureKeyVaultEmulator("keyvault");
+//var keyVault = builder.AddAzureKeyVaultEmulator("keyvault");
 
 // Reference as usual!
 builder
