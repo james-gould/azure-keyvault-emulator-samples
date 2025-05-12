6133baa5d8b25f4d18a9aba831b9ee5d31ccebe7
James Gould <james-gould@users.noreply.github.com>
2025-05-12 10:54:35 +0100

    updated packages and included config in samples

--- dotnet/WebApiWithEmulator-dotnet8/KeyVaultEmulatorSample.AppHost/Program.cs
+++ dotnet/WebApiWithEmulator-dotnet8/KeyVaultEmulatorSample.AppHost/Program.cs
@@ -2,14 +2,24 @@ using AzureKeyVaultEmulator.Aspire.Hosting;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+var options = new KeyVaultEmulatorOptions
+{
+    Persist  = true, // Store data in a reusable database, local on the host machine.
+    LocalCertificatePath = @"C:/MyPath/To/TheFiles", // Configure where the certificates and db are stored.
+    ShouldGenerateCertificates = true, // Should generate SSL certificates for you to install
+    LoadCertificatesIntoTrustStore = true, // Install the SSL certificates automatically into the user trust store.
+    Lifetime = ContainerLifetime.Persistent, // Prevent the container shutting down between sessions,
+    ForceCleanupOnShutdown = true // Unstable, removes all local files from the LocalCertificatePath.
+};
+
 // Redirect existing resource to the emulator:
 
 var keyVault = builder
     .AddAzureKeyVault("keyvault")
-    .RunAsEmulator();
+    .RunAsEmulator(/*options*/);
 
 // Or directly create it without an existing Azure tenancy
-//var keyVault = builder.AddAzureKeyVaultEmulator("keyvault");
+//var keyVault = builder.AddAzureKeyVaultEmulator("keyvault"/*, options*/);
 
 // Reference as usual!
 builder
